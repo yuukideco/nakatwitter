c0b1f5e79408fc77393e7591317fab06ea518d5b
yuukideco <yuukideco20@gmail.com>
2021-01-31 22:54:34 +0900

    投稿フォームをindexページでサインアップユーザーのみに表示する修正を行なった。そのほか諸々デザイン修正。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,10 +4,7 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
-  end
-
-  def new
-   @tweet = Tweet.new
+    @tweet = Tweet.new
   end
 
   def create
